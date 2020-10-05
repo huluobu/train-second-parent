8ce80a43a315c59756bf815f67a2e0bbc7f71cfc
carrot <huluobu.cowork@gmail.com>
2020-10-05 10:53:14 +0800

    构建电影和订单实体类

---
+++ fifthweek-springmvc-homework/src/main/java/com/carrot/train/enetity/FilmMatch.java
@@ -0,0 +1,25 @@
+package com.carrot.train.enetity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @Author: carrot
+ * @Date: 2020/10/5 9:55
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FilmMatch {
+    private Integer id;
+    private Integer filmid;
+    private String location;
+    private String halltype;
+    private Date begindate;
+    private String reseats;
+    private String status;
+
+}
